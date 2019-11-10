6f7fe3c499e9b808fd2e3ffd2169396b1ec74e9f
Brandon Weaver <a7xncob@gmail.com>
2019-11-10 02:34:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,7 @@ class CashRegister
       return "There is no discount to apply."
     end
     @total -= @total * @discount / 100
-    return "After the discount, the total comes to $#{@total}."
+    return "After the discount, the total comes to $#{@total.floor}."
   end
   
   def items
